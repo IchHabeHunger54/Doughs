812f8e2455d5ab6d159df7cbb4d1efcdf9dfe739
IchHabeHunger54 <kingdavidaut@gmail.com>
2020-04-29 18:29:55 +0200

    Initial commit

---
+++ src/main/java/ihh/doughs/Doughs.java
@@ -0,0 +1,22 @@
+package ihh.doughs;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod("doughs")
+public class Doughs {
+    public Doughs() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    public static DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, "doughs");
+    static {
+        ITEMS.register("bread_dough", () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
+        ITEMS.register("cake_dough", () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
+        ITEMS.register("cookie_dough", () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
+        ITEMS.register("pumpkin_pie_dough", () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
+    }
+}

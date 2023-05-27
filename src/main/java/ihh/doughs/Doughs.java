25a11d52010b7a534e56102044c80103ed5123eb
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-05-28 00:30:43 +0200

    update to 1.19.4

--- src/main/java/ihh/doughs/Doughs.java
+++ src/main/java/ihh/doughs/Doughs.java
@@ -1,22 +1,35 @@
 package ihh.doughs;
 
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(Doughs.MOD_ID)
 public class Doughs {
     public static final String MOD_ID = "doughs";
 
     public Doughs() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Doughs.MOD_ID);
-        ITEMS.register("bread_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
-        ITEMS.register("cake_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
-        ITEMS.register("cookie_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
-        ITEMS.register("pumpkin_pie_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        Item.Properties properties = new Item.Properties();
+        RegistryObject<Item> breadDough = ITEMS.register("bread_dough", () -> new Item(properties));
+        RegistryObject<Item> cakeDough = ITEMS.register("cake_dough", () -> new Item(properties));
+        RegistryObject<Item> cookieDough = ITEMS.register("cookie_dough", () -> new Item(properties));
+        RegistryObject<Item> pumpkinPieDough = ITEMS.register("pumpkin_pie_dough", () -> new Item(properties));
+        ITEMS.register(bus);
+        bus.<CreativeModeTabEvent.BuildContents>addListener(e -> {
+            if (e.getTab() == CreativeModeTabs.INGREDIENTS) {
+                e.accept(breadDough);
+                e.accept(cakeDough);
+                e.accept(cookieDough);
+                e.accept(pumpkinPieDough);
+            }
+        });
     }
 }

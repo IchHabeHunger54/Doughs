bc47198639b656ce1aad42c7d2472e8b0f11efa5
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-05-28 00:19:16 +0200

    1.19.2 update? apparently I forgot to commit this lol

--- src/main/java/ihh/doughs/Doughs.java
+++ src/main/java/ihh/doughs/Doughs.java
@@ -7,10 +7,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod("doughs")
+@Mod(Doughs.MOD_ID)
 public class Doughs {
+    public static final String MOD_ID = "doughs";
+
     public Doughs() {
-        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "doughs");
+        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Doughs.MOD_ID);
         ITEMS.register("bread_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
         ITEMS.register("cake_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
         ITEMS.register("cookie_dough", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));

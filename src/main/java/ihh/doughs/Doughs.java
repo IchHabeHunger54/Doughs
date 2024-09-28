792e3148e2fa287131cf61e8270eaeb0aaec4879
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-09-28 17:57:37 +0200

    update to 1.21.1

--- src/main/java/ihh/doughs/Doughs.java
+++ src/main/java/ihh/doughs/Doughs.java
@@ -2,26 +2,22 @@ package ihh.doughs;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(Doughs.MOD_ID)
 public class Doughs {
     public static final String MOD_ID = "doughs";
 
-    public Doughs() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Doughs.MOD_ID);
-        Item.Properties properties = new Item.Properties();
-        RegistryObject<Item> breadDough = ITEMS.register("bread_dough", () -> new Item(properties));
-        RegistryObject<Item> cakeDough = ITEMS.register("cake_dough", () -> new Item(properties));
-        RegistryObject<Item> cookieDough = ITEMS.register("cookie_dough", () -> new Item(properties));
-        RegistryObject<Item> pumpkinPieDough = ITEMS.register("pumpkin_pie_dough", () -> new Item(properties));
+    public Doughs(IEventBus bus) {
+        DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
+        DeferredItem<Item> breadDough = ITEMS.registerSimpleItem("bread_dough");
+        DeferredItem<Item> cakeDough = ITEMS.registerSimpleItem("cake_dough");
+        DeferredItem<Item> cookieDough = ITEMS.registerSimpleItem("cookie_dough");
+        DeferredItem<Item> pumpkinPieDough = ITEMS.registerSimpleItem("pumpkin_pie_dough");
         ITEMS.register(bus);
         bus.<BuildCreativeModeTabContentsEvent>addListener(e -> {
             if (e.getTabKey() == CreativeModeTabs.INGREDIENTS) {
